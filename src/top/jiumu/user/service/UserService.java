3fd8130bf78eb946318c2e3ed0c61821f67464b9
jiumuo <39945562+jiumuo@users.noreply.github.com>
2018-06-06 01:24:36 +0800

    把用户登陆信息校验修改为在service层进行
    把用户登陆信息校验修改为在service层进行

--- src/top/jiumu/user/service/UserService.java
+++ src/top/jiumu/user/service/UserService.java
@@ -30,25 +30,29 @@ public class UserService {
 	
 	/**
 	 * 登录功能
+	 * @param form
+	 * @return
+	 * @throws UserException 
 	 */
-	public User login(User user){
-		User _user = userDao.findByUsername(user.getUsername());
-		return _user;
+	public User login(User form) throws UserException {
+		/*
+		 * 1. 使用form中的username进行查询，得到User user
+		 */
+		User user = userDao.findByUsername(form.getUsername());
+		/*
+		 * 2. 如果返回null，说明用户名不存在，抛出异常，异常信息为“用户名不存在”
+		 */
+		if(user == null) throw new UserException("用户名不存在！");
+		/*
+		 * 3. 比较user的password和form的password，如果不同，抛出异常，异常信息为“密码错误！”
+		 */
+		if(!form.getPassword().equals(user.getPassword())) {
+			throw new UserException("密码错误！");
+		}
+		
+		/*
+		 * 返回数据库中查询出来的user，而不是form，因为form中只有用户名和密码，而user是所有用户信息！
+		 */
+		return user;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
